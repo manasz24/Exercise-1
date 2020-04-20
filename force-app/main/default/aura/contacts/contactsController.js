50a2c145c67d858a5408068e4a62d0349d90ce38
Manas Datta <manasdatta.cs@gmail.com>
2020-04-20 20:39:00 +0800

    commit 2

--- force-app/main/default/aura/contacts/contactsController.js
+++ force-app/main/default/aura/contacts/contactsController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    goToRecord : function(component, event, helper) {
+        // Fire the event to navigate to the contact record
+        var sObjectEvent = $A.get("e.force:navigateToSObject");
+        sObjectEvent.setParams({
+            "recordId": component.get("v.contact.Id")
+        })
+        sObjectEvent.fire();
     }
 })
